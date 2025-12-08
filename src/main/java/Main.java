75bef9dddae3f842afacd4b65d9455760975446c
Abdullah Ahmed <abdol-ahmed@users.noreply.github.com>
2025-12-08 15:08:38 -0800

    implement support for printing error messages for invalid commands.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        switch (command) {
+            case "exit":
+                System.out.println("Goodbye!");
+                System.exit(0);
+            default:
+                System.out.printf(command + ": command not found");
+                System.exit(0);
+        }
     }
 }
