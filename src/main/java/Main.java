9f4f2ad6abcfde5090cd8020adcf5aa004170f9b
Abdullah Ahmed <abdol-ahmed@users.noreply.github.com>
2025-12-08 16:50:53 -0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String command = scanner.nextLine();
+            String[] arguments = scanner.nextLine().trim().split(" ", 2);
+            String command = arguments[0];
+            String arg = arguments.length > 1 ? arguments[1] : "";
             switch (command) {
+                case "echo":
+                    System.out.println(arg);
+                    break;
                 case "exit":
                     System.exit(0);
                 default:
