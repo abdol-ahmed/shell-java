015991f25f7adc60be31b5ded1653ff5e8c2508c
Abdullah Ahmed <abdol-ahmed@users.noreply.github.com>
2025-12-12 22:58:03 -0800

    codecrafters submit [skip ci]

--- src/main/java/ChangeDirectory.java
+++ src/main/java/ChangeDirectory.java
@@ -1,8 +1,11 @@
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class    ChangeDirectory implements Command {
     @Override
     public boolean execute(String[] args) {
+
         String arg = args.length > 1 ? args[1] : "";
         if (arg.isEmpty()) {
             System.out.println(System.getProperty("user.home"));
@@ -10,12 +13,16 @@ public class    ChangeDirectory implements Command {
             return true;
         }
 
-        File file = new File(arg);
+        Path inputPath = Paths.get(arg);
+        Path cwd = Paths.get(System.getProperty("user.dir"));
+        String absolutePath = cwd.resolve(inputPath).normalize().toString();
+
+        File file = new File(absolutePath);
         if (file.exists() && file.isDirectory()) {
-            System.setProperty("user.dir", arg);
+            System.setProperty("user.dir", absolutePath);
             return true;
         } else {
-            System.out.println("cd: " + arg + ": No such file or directory");
+            System.out.println("cd: " + absolutePath + ": No such file or directory");
         }
         return true;
     }
